e0a54ca2e69349f4128d53ab64673735976524e5
Maruf <zahidulmaruf13@gmail.com>
2024-09-13 20:47:32 +0600

    display Product Page Update

--- src/Pages/Display Product/DisplayProduct.jsx
+++ src/Pages/Display Product/DisplayProduct.jsx
@@ -9,29 +9,33 @@ const DisplayProduct = () => {
     const populer = product.filter(item=> item.view === 'popular')
     const tranding = product.filter(item=> item.view === 'tranding')
     const newComming = product.filter(item=> item.view === 'new')
-    console.log(product.length,populer,tranding,newComming)
-
+    
 
     return (
         <div >
-          <SectionTitle heading={'New Arrivals' } subHeading={'Check it Out'}></SectionTitle>     
+
+          <SectionTitle heading={'New Arrivals' } subHeading={'Check it Out'}></SectionTitle> 
+
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2  md:gap-4">
             {
                 newComming.map(item=> <ItemCard key={item._id} item={item} ></ItemCard>)
             }
           </div>
+
           <SectionTitle heading={'Trading Now' } subHeading={'Check it Out'}></SectionTitle>     
           <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4">
             {
                 tranding.map(item=> <ItemCard key={item._id} item={item} ></ItemCard>)
             }
           </div>
+
           <SectionTitle heading={'Populer Now' } subHeading={'Check it Out'}></SectionTitle>     
           <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4">
             {
                 populer.map(item=> <ItemCard key={item._id} item={item} ></ItemCard>)
             }
           </div>
+          
         </div>
     );
 };
